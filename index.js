8de4e2b9eaa878c07db3c19dd20fab3f27146e5f
chalpenny <cp.halpenny@gmail.com>
2020-06-11 23:23:21 -0700

    initialize get fetch request

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("testing")
+const BACKEND_URL = 'http://localhost:3000'
+const CATEGORIES_URL = `${BACKEND_URL}/categories`
+const ACTIVITIES_URL = `${BACKEND_URL}/activities`
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+   getCategoriesList()
+})
+
+function getCategoriesList(){
+    fetch(CATEGORIES_URL)
+    .then(response => response.json())
+    .then(category => {
+    console.log(category)
+ 
+    })
+    console.log("loaded")
+}
