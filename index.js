1889bac11115f37640c5f4c41a1c77a13784d51b
chalpenny <cp.halpenny@gmail.com>
2020-06-12 00:27:35 -0700

    make category list

--- index.js
+++ index.js
@@ -11,9 +11,24 @@ document.addEventListener("DOMContentLoaded", () => {
 function getCategoriesList(){
     fetch(CATEGORIES_URL)
     .then(response => response.json())
-    .then(category => {
-    console.log(category)
- 
-    })
-    console.log("loaded")
+    .then(categories => {
+    for (category of categories)
+    makeCategoryList(category)
+    })  
+    // make Add A Category button function to go here
+
 }
+
+function makeCategoryList(category) {
+    /* make const in htmal file for category to attach to
+        attach cat element to const
+        attach cat el const to html const
+    */
+    const categoryList = document.getElementById("category-list")
+    const categoryElement = document.createElement("h2") 
+    categoryElement.innerText = category.name
+    categoryList.appendChild(categoryElement)
+
+    console.log(category)
+}
+
