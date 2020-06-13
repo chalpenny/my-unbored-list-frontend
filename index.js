e4a502f2a42fdbb5caaaa947904e8b60b287367c
chalpenny <cp.halpenny@gmail.com>
2020-06-13 13:04:13 -0700

    add category list click functionality - edit, delete, activities function

--- index.js
+++ index.js
@@ -5,33 +5,55 @@ const ACTIVITIES_URL = `${BACKEND_URL}/activities`
 
 
 document.addEventListener("DOMContentLoaded", () => {
-   getCategoriesList()
+   getCategories()
 })
 
-function getCategoriesList(){
+function getCategories(){
     fetch(CATEGORIES_URL)
     .then(response => response.json())
     .then(categories => {
     for (category of categories)
     makeCategoryList(category)
+    //activities edit, delete
     })  
 }
 
 function makeCategoryList(category){
+
+    //LIST CATEGORIES
     const categoryList = document.getElementById("category-list")
-    const categoryListBr = document.createElement("br")
-
-    const categoryElement = document.createElement("a") 
-    categoryElement.setAttribute("href", CATEGORIES_URL)
+    const categoryElement = document.createElement("h2") 
     categoryElement.innerText = category.name
+    categoryList.appendChild(categoryElement)
 
-    categoryList.appendChild(categoryElement).appendChild(categoryListBr)
+    //EDIT BUTTON
+    const editButton = document.createElement("button")
+    editButton.classList += "edit"
+    editButton.setAttribute("edit-category", category.id)
+    editButton.innerText = "edit"
+    categoryElement.appendChild(editButton)
 
-    showActivities()
+    //DELETE BUTTON
+    const deleteButton = document.createElement("button")
+    deleteButton.classList += "delete"
+    deleteButton.setAttribute("delete-category", category.id)
+    deleteButton.innerText = "delete"
+    categoryElement.appendChild(deleteButton)
+
+    //SHOW ACTIVITIES ON CLICK
+    categoryElement.addEventListener("click", () => {
+        const activitiesList = document.createElement("li")
+        activitiesList.classList += "activities"
+        activitiesList.setAttribute("parent-category", category.id)
+        activitiesList.innerText = " activity here" 
+            //  iterate over category.activity to display name, url, notes */
+
+        categoryElement.appendChild(activitiesList) 
+    })
+
 }
-
-function showActivities(){
-    console.log("ready for activities")
-        /* add event listener on click, prevent_default action 
-        iterate over category to display category.activity */
-}
+ //Add URL
+   // categoryElement.addEventListener("click", showActivities(category))
+    // const categoryElement = document.createElement("a") 
+    // categoryElement.setAttribute("href", CATEGORIES_URL)
+    // categoryElement.innerText = category.name
